953d281a517bdde09bcb61b4dd074bdd11d2d2d8
bianca aguinaga <bianca.aguinaga.pizarro@gmail.com>
2023-08-19 21:48:19 -0500

    estructura

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include <ordenamiento.h>
+#define ll long long 
+using namespace std;
+
+int main(){
+    ll n;
+    cin >> n;
+    
+    return 0;
+}
---
+++ ordenamiento.h
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+template<typename T>
+class ordenamiento{
+private:
+    ll size;
+    ll array;
+
+public:
+    ordenamiento(ll n){
+        size = n;
+        llenar();
+    }
+
+    void quick_sort(){
+
+    }
+
+    void merge_sort(){
+
+    }
+};
