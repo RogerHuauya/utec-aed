e443620825645853c5044f383ba94a262901641d
bianca aguinaga <bianca.aguinaga.pizarro@gmail.com>
2023-08-20 02:09:54 -0500

    no recuerdo sobre clases :( + insertion sort

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <bits/stdc++.h>
-#include <ordenamiento.h>
+#include "ordenamiento.h"
 #define ll long long 
 using namespace std;
 
 int main(){
     ll n;
     cin >> n;
+
+    Ordenamiento ord(n);
+    ord.insertion_sort();
+    ord.print();
     
     return 0;
 }
--- ordenamiento.h
+++ ordenamiento.h
@@ -1,25 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 #define ll long long
+const ll MAX = 1e6+10;
 
-template<typename T>
-class ordenamiento{
+class Ordenamiento{
 private:
-    ll size;
-    ll array;
+    int sz;
+    ll array[MAX];
+    
+    void llenar(){
+        random_device rd;
+        uniform_int_distribution<int> dist(1,100);
+        for(int i = 0;i < sz; i++)
+            array[i] = dist(rd);  
+    }
+
+    int partition(int l, int r){
+        int pivot = array[l];
+        int aux = r;
+        for(int i = r; i > l; i--){
+            if(array[i] > pivot)
+                swap(array[i],array[aux--]);
+        }
+        swap(array[l],array[aux]);
+        return aux;
+    }
 
 public:
-    ordenamiento(ll n){
-        size = n;
+    Ordenamiento(int n){
+        sz = n;
         llenar();
     }
 
-    void quick_sort(){
-
+    void quicksort(int l,int r){
+        if(l<r){
+            int p = partition(l,r);
+            quicksort(l,p-1);
+            quicksort(p+1,r);
+        }
     }
 
     void merge_sort(){
+        
+    }
 
+    void insertion_sort(){
+        for (int i = 1; i < sz; i++){
+            int j = i-1, cur = array[i];
+
+            while(j >= 0 && array[j] > cur){
+                array[j+1] = array[j];
+                j--;
+            }
+            array[j+1] = cur;
+        }
+        
+    }
+
+    void print(){
+        for(int i = 0; i < sz; i++) cout << array[i] << " ";
+        cout << endl;
     }
 };
