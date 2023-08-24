8fd38c044ea0a649cb581550f963ee9bde33afcb
Roger <rhuauyam@uni.pe>
2023-08-24 18:14:17 -0500

    plot changed

--- SortingPlot.py
+++ SortingPlot.py
@@ -26,21 +26,43 @@ def main():
                                 "MergeSort_results.csv",
                                 "InsertionSort_results.csv",
                                 ]
-
-    for (algorithm, file_name) in zip(sorting_algorithms,
-                                      sorting_algorithms_files):
-        plt.figure()
-        data = pd.read_csv("cmake-build-debug/" + file_name)
-        plt.style.use('ggplot')
-        plt.title(algorithm)
-        plt.xlabel("List Size")
-        plt.ylabel("Time")
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 6))
+    ax = axes[0]
+    for (algorithm, file_name) in zip(sorting_algorithms[:2], sorting_algorithms_files[:2]):
+        data = pd.read_csv(file_name)
+        ax.set_title("Quicksort vs Mergesort")
+        ax.set_xlabel("List Size")
+        ax.set_ylabel("Time")
         x = np.log10(data["n"])
         y = data["tiempo"]
-        plt.plot(x, y, label=algorithm)
-        plt.scatter(x, y, color='red', marker='o', label='Points')
-        plt.savefig(file_name[:-4] + ".png")
+        ax.plot(x, y, label=algorithm)
+        ax.scatter(x, y, color='red', marker='o')
 
+    ax.set_title("QuickSort vs MergeSort")
+    ax.set_xlabel("List Size")
+    ax.set_ylabel("Time")
+    ax.legend()
+
+    ax = axes[1]
+    algorithm = sorting_algorithms[2]
+    file_name = sorting_algorithms_files[2]
+    data = pd.read_csv(file_name)
+    ax.set_title(algorithm)
+    ax.set_xlabel("List Size")
+    ax.set_ylabel("Time")
+    x = np.log10(data["n"])
+    y = data["tiempo"]
+    ax.plot(x, y, label=algorithm)
+    ax.scatter(x, y, color='red', marker='o')
+
+    ax.set_title(algorithm)
+    ax.set_xlabel("List Size")
+    ax.set_ylabel("Time")
+    ax.legend()
+
+    plt.tight_layout()
+    plt.tight_layout()
+    plt.savefig("sorting_algorithms_subplot.png")
     plt.show()
 
 if __name__ == "__main__":
